06c6beaf4a38e181b280043874d2c4353b083fd1
aissa <ouboukioudaissa@gmail.com>
2021-01-22 18:47:38 +0100

    fix the problem in the loging.js file because of the -git pull- command cause

--- app/static/app/js/login.js
+++ app/static/app/js/login.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     $(".create_new_account_btn").on("click", function (e) {
         e.preventDefault();
         $('.register_form_wrapper').css('display', 'block');
-<<<<<<< HEAD
     });
     // $('.register_form_wrapper').on('click', function(e) {
     // 	$(this).css('display', 'none');
@@ -16,8 +15,6 @@ $(document).ready(function () {
     // });
     $('.header_right').on('click', function(e) {
         $('.register_form_wrapper').css('display', 'none');
-=======
->>>>>>> 48c4008d15b861d36d9d0341183f36a460a1011f
     });
     $('.register_form_wrapper').on('click', function(e) {
     	$(this).css('display', 'none');
