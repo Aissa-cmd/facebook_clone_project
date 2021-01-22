d4aba11f0024da24aa9a962c540c3c40dc20c1bd
aissa <ouboukioudaissa@gmail.com>
2021-01-22 18:51:38 +0100

    fix all the problems in the login.js file

--- app/static/app/js/login.js
+++ app/static/app/js/login.js
@@ -3,27 +3,7 @@ $(document).ready(function () {
         e.preventDefault();
         $('.register_form_wrapper').css('display', 'block');
     });
-    // $('.register_form_wrapper').on('click', function(e) {
-    // 	$(this).css('display', 'none');
-    // });
-    // $('.register_form_container').on('click', function(e) {
-    // 	if(e.stopPropagation) {
-    // 		e.stopPropagation()
-    // 	} else {
-    // 		e.cancelBubble = true;
-    // 	}
-    // });
     $('.header_right').on('click', function(e) {
         $('.register_form_wrapper').css('display', 'none');
-    });
-    $('.register_form_wrapper').on('click', function(e) {
-    	$(this).css('display', 'none');
-    })
-    $('.register_form_container').on('click', function(e) {
-    	if(e.stopPropagation) {
-    		e.stopPropagation()
-    	} else {
-    		e.cancelBubble = true;
-    	}
     })
 });
