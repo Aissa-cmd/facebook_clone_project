08c0e12249814ee37d11a7d1d264ff1f783edf78
aissa <ouboukioudaissa@gmail.com>
2021-01-22 18:42:32 +0100

    complete the design of the register form

--- app/static/app/js/login.js
+++ app/static/app/js/login.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
     $(".create_new_account_btn").on("click", function (e) {
         e.preventDefault();
-        console.log("click");
+        $('.register_form_wrapper').css('display', 'block');
+    });
+    // $('.register_form_wrapper').on('click', function(e) {
+    // 	$(this).css('display', 'none');
+    // });
+    // $('.register_form_container').on('click', function(e) {
+    // 	if(e.stopPropagation) {
+    // 		e.stopPropagation()
+    // 	} else {
+    // 		e.cancelBubble = true;
+    // 	}
+    // });
+    $('.header_right').on('click', function(e) {
+        $('.register_form_wrapper').css('display', 'none');
     });
 });
