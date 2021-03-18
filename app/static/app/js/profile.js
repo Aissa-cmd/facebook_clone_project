1a9b2c5047ca27d7fa12a741d22f477aa9cafe89
aissa <aissa.1bacsm1@gmail.com>
2021-03-18 12:17:08 +0100

    commiting the last changing and complete working on the procject

--- app/static/app/js/profile.js
+++ app/static/app/js/profile.js
@@ -4,11 +4,20 @@ $(document).ready(function () {
     	$('.option_info_module').text($(this).attr('data-icon-name'));
     	$('.option_info_module').css('top', module_top);
     	let module_left = this.getBoundingClientRect().left;
-    	$('.option_info_module').css('left', module_left);
+        // if(module_left + parseInt($('.option_info_module').css('width')) > widow.innerWidth) {
+        //     $('.option_info_module').css('right', "15");
+        // } else {
+        //     $('.option_info_module').css('left', module_left);
+        // }
+        $('.option_info_module').css('left', module_left);
     	$('.option_info_module').css('display', 'block');
     })
     $('.option_hover').on('mouseleave', function(e) {
     	$('.option_info_module').css('display', 'none');
+    })
+
+    $(".right_options_menu").on("click", function(e) {
+        $(".nav_right_options_module").css("display" : "block");
     })
     // $('.post_options_list').on('click', function() {
     //     console.log('click');
